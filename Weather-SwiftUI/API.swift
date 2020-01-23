13c7c8d99a510929daeb433749bb88e3bb0e6e83
Imran <imufun2@gmail.com>
2020-01-23 23:48:38 +0600

    first commit

---
+++ Weather-SwiftUI/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  Weather-SwiftUI
+//
+//  Created by Sinbad on 23/1/20.
+//  Copyright © 2020 Sinbad. All rights reserved.
+//
+
+import Foundation
---
+++ Weather-SwiftUI/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  Weather-SwiftUI
+//
+//  Created by Sinbad on 23/1/20.
+//  Copyright © 2020 Sinbad. All rights reserved.
+//
+
+import Foundation
